41517795b5baf91623a00cb38080f4edae4f3a9e
Nischal <nischalbasnet7848@gmail.com>
2025-04-10 23:40:30 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import { createBrowserRouter } from 'react-router';
 import RootLayout from './components/RootLayout';
 import TodoPage from './pages/todo/TodoPage';
 import { RouterProvider } from 'react-router-dom';
-import { todoSlice } from './pages/todo/todoSlice';
 import TodoAdd from './pages/todo/TodoAdd';
+import TodoEdit from './pages/todo/TodoEdit';
 
 export default function App() {
-  console.log(todoSlice);
-
   const router = createBrowserRouter([
     {
       path: '/',
@@ -21,6 +19,10 @@ export default function App() {
         {
           path: 'add-todo',
           element: <TodoAdd />,
+        },
+        {
+          path: 'edit-todo/:id',
+          element: <TodoEdit />,
         },
       ],
     },
