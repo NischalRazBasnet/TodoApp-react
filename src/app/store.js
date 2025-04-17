3daa26b38c4bebd0db1ad4238c803432c720b4aa
Nischal <nischalbasnet7848@gmail.com>
2025-04-17 08:20:32 +0545

    changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { todoSlice } from '../pages/todo/todoSlice';
 import { postSlice } from '../pages/todo/postSlice';
+import { counterSlice } from '../pages/todo/counterSlice';
 
 export const store = configureStore({
   reducer: {
     [todoSlice.name]: todoSlice.reducer,
     [postSlice.name]: postSlice.reducer,
+    [counterSlice.name]: counterSlice.reducer,
   },
 });
